dcbda49574896c0707b6b74d1298c73a67574ed7
Jake-G123 <jakelgerken@gmail.com>
2025-10-30 14:12:12 -0700

    Completed second problem

--- src/ProblemSolving.java
+++ src/ProblemSolving.java
@@ -35,6 +35,14 @@ public class ProblemSolving {
    * @return true if there is at least one empty string, false otherwise
    */
   public static boolean hasEmptyString(Set<String> words) {
+    if (words.isEmpty()) {
+      return false;
+    }
+    for (String word : words) {
+      if (word.isEmpty()) {
+        return true;
+      }
+    }
     return false;
   }
 
